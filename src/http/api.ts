d4c1052260f7ec3102a1bea63c13e11267894896
Raj Singh <raj02mar2001@gmail.com>
2025-02-01 22:12:15 +0530

    add login endpoint

---
+++ src/http/api.ts
@@ -0,0 +1,7 @@
+import { Credentials } from "../types";
+import { api } from "./client";
+
+// Auth service
+
+export const login = (credentials: Credentials) =>
+  api.post("/auth/login", credentials);
