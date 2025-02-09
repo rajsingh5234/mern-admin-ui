a91ed3b6bd0ecbebf962522c9fd7267c9df2a88f
Raj Singh <raj02mar2001@gmail.com>
2025-02-09 13:19:48 +0530

    hide restaurants for manager users

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -22,11 +22,6 @@ const getMenuItems = (role: string) => {
             label: <NavLink to="/">Home</NavLink>
         },
         {
-            key: '/restaurants',
-            icon: <Icon component={FoodIcon} />,
-            label: <NavLink to="/restaurants">Restaurants</NavLink>
-        },
-        {
             key: '/products',
             icon: <Icon component={BasketIcon} />,
             label: <NavLink to="/products">Products</NavLink>
@@ -44,6 +39,12 @@ const getMenuItems = (role: string) => {
             key: '/users',
             icon: <Icon component={UserIcon} />,
             label: <NavLink to="/users">Users</NavLink>,
+        });
+
+        menus.splice(2, 0, {
+            key: '/restaurants',
+            icon: <Icon component={FoodIcon} />,
+            label: <NavLink to="/restaurants">Restaurants</NavLink>
         });
 
         return menus;
