77c421ff9c0f4530060cfcadc8b06cdf2f48e2c7
Raj Singh <raj02mar2001@gmail.com>
2025-02-09 13:14:29 +0530

    fix url redirect

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, NavLink, Outlet } from "react-router-dom"
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom"
 import { useAuthStore } from "../store"
 import { Avatar, Badge, Dropdown, Flex, Layout, Menu, Space, theme } from "antd";
 import Icon, { BellFilled } from '@ant-design/icons'
@@ -53,7 +53,7 @@ const getMenuItems = (role: string) => {
 };
 
 const Dashboard = () => {
-
+    const location = useLocation();
     const { user, logout: logoutFromStore } = useAuthStore();
 
     const { mutate: logoutMutate } = useMutation({
@@ -72,7 +72,7 @@ const Dashboard = () => {
     } = theme.useToken();
 
     if (user === null) {
-        return <Navigate to="/auth/login" replace={true} />
+        return <Navigate to={`/auth/login?returnTo=${location.pathname}`} replace={true} />
     }
 
     const items = getMenuItems(user.role);
